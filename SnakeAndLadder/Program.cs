92b4cc4e2aff6f9b604cf312d0df4f28beb569ab
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-17 21:48:44 +0530

    [yuvanthika] Add :created

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-namespace SnakeAndLadder
+
+namespace SnakeAndLadderPrg
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and ladder Program");
+            Console.WriteLine("Welcome to Snake and Ladder Program");
+
             SnakeAndLadder snake = new SnakeAndLadder();
             snake.DieRoll();
-            snake.Game();
+            int player1 = snake.Game();
+            SnakeAndLadder snake1 = new SnakeAndLadder();
+            snake1.DieRoll();
+            int player2 = snake1.Game();
 
+            Console.WriteLine("Player1 played: " + player1 + " times " + "\nPlayer2 played: " + player2 + "times");
+
+            if (player1.CompareTo(player2) > 0)
+            {
+                Console.WriteLine("Player 2");
+            }
+            else if (player1.CompareTo(player2) < 0)
+            {
+                Console.WriteLine("Player 1 ");
+            }
+            else
+            {
+                Console.WriteLine("Draw");
+            }
         }
     }
 }
--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -5,36 +5,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeAndLadder
+namespace SnakeAndLadderPrg
 {
     public class SnakeAndLadder
     {
         const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100, STARTING_POSITION = 0;
         int position = 0, count = 0;
+        bool WON = false;
 
         Random random = new Random();
         public int DieRoll()
         {
             int diePosition = random.Next(1, 7);
-            Console.WriteLine("Player Position" + " " + this.position);
             count++;
+
+            Console.Write(this.position + "| ");
             return diePosition;
         }
-        public void Game()
+        public bool GetWon()
+        {
+            return WON;
+        }
+        public int Game()
         {
             while (this.position < WINNING_POSITION)
             {
+
                 int option = random.Next(0, 3);
                 switch (option)
                 {
+
                     case NO_PLAY:
                         break;
                     case LADDER:
                         int dieRoll = DieRoll();
-                        if (this.position + dieRoll < WINNING_POSITION)
+                        if (this.position + dieRoll <= WINNING_POSITION)
                         {
                             this.position += dieRoll;
                         }
+
                         break;
                     case SNAKE:
                         dieRoll = DieRoll();
@@ -46,10 +55,19 @@ namespace SnakeAndLadder
                         {
                             this.position -= dieRoll;
                         }
+
                         break;
                 }
             }
-            Console.WriteLine("Number of times the dice's played" + " " + count);
+
+            if (this.position == WINNING_POSITION)
+            {
+                WON = true;
+            }
+
+
+            Console.WriteLine("\nNumber of times dice's played: " + count);
+            return count;
         }
     }
 }
